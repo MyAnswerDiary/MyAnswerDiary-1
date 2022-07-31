83f70f6f4644900f235c09d31c9cb8d2dd489c63
Suanna01 <yangnony01@gmail.com>
2022-07-31 17:32:18 +0900

    회원가입 진행 중

--- MyAnswerDiaryPrj/MyAnswerDiaryPrj/settings.py
+++ MyAnswerDiaryPrj/MyAnswerDiaryPrj/settings.py
@@ -29,6 +29,18 @@ INSTALLED_APPS = [
     'myAnswerDiaryApp',
 ]
 
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+LOGIN_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
